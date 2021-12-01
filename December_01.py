4b82e20cbfa91bc16ee2a04e6b258b5234fc7b9d
RCBSi <45831224+RCBSi@users.noreply.github.com>
2021-12-01 08:49:40 +0200

    Update December_01.py

--- December_01.py
+++ December_01.py
@@ -1,19 +1,14 @@
-# advent-of-code-2021
-
-import math 
-
 def up(x,y): # boolean "is this an increase"
     if y > x: 
         return 1
     else:
         return 0
 
+def up_seq(t,k): # k = offset.
+    return [up(t[i],t[i+k]) for i in range(len(t)-k)]
+
 with open('day01v1.txt', 'r') as file:
     t = [int(x.strip()) for x in file.readlines()]
 
-mary = [up(t[i],t[i+1]) for i in range(len(t)-1)]
-mary2 = [up(t[i],t[i+3]) for i in range(len(t)-3)]
-
-print(sum(mary))
-print(sum(mary2))
-
+print("part1:",sum(up_seq(t,1)))
+print("part2:",sum(up_seq(t,3)))
