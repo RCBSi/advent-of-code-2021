0729399cade9f031a4183588b6314aaebecd4ab3
RCBSi <45831224+RCBSi@users.noreply.github.com>
2021-12-09 13:44:53 +0200

    Update December_08.py

--- December_08.py
+++ December_08.py
@@ -1,32 +1,51 @@
-def score(u):
+def score(u,f):
     [ls,rs] = [u.split()[:10],u.split()[11:15]]
     ls = ["".join(sorted(x)) for x in ls]
     rs = ["".join(sorted(x)) for x in rs]
 
     con = [(sm,bi) for sm in ls for bi in ls if 
         len(bi) > len(sm) and len(bi)-len(sm) <= 2 and 
-        sum([i in bi for i in sm])== len(sm)] # "containment" 
+        sum([i in bi for i in sm])== len(sm)] # "containment" as a graph.
 
-    num_kids = {y:0 for y in ls} 
+    nk = {y:0 for y in ls} # number of kids.
     for (x,y) in con:
-        num_kids[y] += 1
+        nk[y] += 1
 
-    num_pare = {x:0 for x in ls} #parents
+    np = {x:0 for x in ls} # number of parents.
     for (x,y) in con:
-        num_pare[x] += 1
+        np[x] += 1
 
-    lenu = {2:1,4:4,3:7,7:8} # le = length; wi = wirset; nu = number.
-    winu = {s:lenu[len(s)] for s in ls if len(s) in lenu} 
-    winu[[s for s in ls if len(s) == 6 and num_kids[s] == 0][0]] = 0
-    winu[[s for s in ls if len(s) == 5 and num_pare[s] == 1][0]] = 2
-    winu[[j for (i,j) in con if len(i) == 3][0]] = 3     
-    winu[[s for s in ls if len(s) == 5 and num_pare[s] == 3][0]] = 5
-    winu[[s for s in ls if len(s) == 6 and num_kids[s] == 1][0]] = 6   
-    winu[[j for (i,j) in con if len(i) == 4][0]] = 9
+    winu = {s : f[(nk[s], np[s], len(s))] for s in ls}
 
     return sum([winu[rs[i]]*(10**(3-i)) for i in range(len(rs))])
 
 with open('day08v1.txt', 'r') as file:
     t = [x.strip() for x in file.readlines()]
 
-print(sum([score(u) for u in t]))
+f ={
+    (0, 1, 5): 2,
+    (0, 1, 6): 0,
+    (0, 2, 2): 1,
+    (0, 3, 5): 5,
+    (1, 1, 3): 7,
+    (1, 1, 4): 4,
+    (1, 1, 6): 6,
+    (1, 2, 5): 3,
+    (3, 1, 6): 9,
+    (6, 0, 7): 8}
+
+print(sum([score(u,f) for u in t]))
+
+#d = pd.DataFrame(num_kids.items(), columns=["id","kids"])
+#d["pare"] = d.id.apply(lambda x: num_pare.get(x,-1))
+#d["numb"] = d.id.apply(lambda x: winu.get(x,-1))
+#d["len"] = d.id.apply(lambda x:len(x))
+#d.groupby(["kids","pare","len"]).numb.min().to_dict()
+#d.numb by hand: 
+# 2 has no children and only 8 as a parent; 
+# 0 has no children and only 8 as a parent;
+# 1 has no children and only 4,7 as parent;
+# 5 has no children and 6,8,9 as parents
+# Huom:
+#  parent-child relationships with 
+#  len(parent) = len(child) + 2 are rare. 
