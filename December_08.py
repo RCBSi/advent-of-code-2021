d4b16df04300ad119295886f1eced756e80e7914
RCBSi <45831224+RCBSi@users.noreply.github.com>
2021-12-09 13:50:24 +0200

    Update December_08.py

--- December_08.py
+++ December_08.py
@@ -35,3 +35,21 @@ f ={
     (6, 0, 7): 8}
 
 print(sum([score(u,f) for u in t]))
+
+#import pandas
+#d = pd.DataFrame(nk.items(), columns=["id","kids"])
+#d["pare"] = d.id.apply(lambda x: np.get(x,-1))
+#d["len"] = d.id.apply(lambda x:len(x))
+#d.groupby(["kids","pare","len"]).numb.min().to_dict()
+#d.numb by hand: 
+# 2 has no children and only 8 as a parent; 
+# 0 has no children and only 8 as a parent;
+# 1 has no children and only 4,7 as parent;
+# 5 has no children and 6,8,9 as parents
+# 7 has child 1 and parent 3
+# 4 has child 1 and parent 9 ...
+# Huom:
+#  parent-child relationships with 
+#  len(parent) = len(child) + 2 are helpful!
+# Image:
+#  1 < 7 < 3 < 9 < 8
