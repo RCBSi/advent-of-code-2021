0025e3cedd4e2da48934f34fa682eca12150aa52
RCBSi <45831224+RCBSi@users.noreply.github.com>
2021-12-20 19:57:00 +0200

    Update December_19.py

--- December_19.py
+++ December_19.py
@@ -1,4 +1,6 @@
-uni = [ #these are the rotations of 3-space preserving {x,y,z,-x,-y,-z}
+import time
+start = time.time()
+uni = [ # 24 rotations 
     [[-1, 0, 0], [0, -1, 0], [0, 0, 1]],
     [[-1, 0, 0], [0, 0, -1], [0, -1, 0]],
     [[-1, 0, 0], [0, 0, 1], [0, 1, 0]],
@@ -34,51 +36,44 @@ def mm(w,x): #matrix multiplicaiton.
     (n,m) = (range(len(w)), range(len(x[0])))
     return [[dot(w[i],col(x,j)) for j in m] for i in n]
 
-def rev(te): # read scanner-beacon vectors. 
-    so = [] # star-observation, a list of stars, indexed by observer-number.
-    soo = [] # a list of lists of star-observations.
-    for li in te:
-        if len(li) == 0:
-            soo.append(so)
+def rev(te): # read vectors
+    so, soo = [], [] # star-observations per observer.
+    for li in te[1:]:
+        if ',' in li:
+            so.append([int(x) for x in li.split(",")])
         elif li[:12] == '--- scanner ':
+            soo.append(so)
             so = []
-        else:
-            os = [int(x) for x in li.split(",")] # vector from observer to star.
-            so.append(os)
     soo.append(so)
     return soo
 
 def fi(obl): #fingerprints based on the two nearest stars.
     dps = {}
     for [x,y,z] in obl: 
-        dpsx = [sum([(a-x)**2,(b-y)**2,(c-z)**2]) for [a,b,c] in obl]
+        dpsx = [[sum([(a-x)**2,(b-y)**2,(c-z)**2]),[a,b,c]] for [a,b,c] in obl]
         dpsx.sort()
-        nn2 = [[a,b,c] for [a,b,c] in obl if sum([(a-x)**2,(b-y)**2,(c-z)**2]) == dpsx[1]]+[
-            [a,b,c] for [a,b,c] in obl if sum([(a-x)**2,(b-y)**2,(c-z)**2]) == dpsx[2]
-        ]
+        nn2 = [dpsx[i][1] for i in range(1,3)]
         dpnn2 = abs((nn2[0][0]-x)*(nn2[1][0]-x) + (nn2[0][1]-y)*(nn2[1][1]-y) + (nn2[0][2]-z)*(nn2[1][2]-z)) # dot product to the nearest two neighbours. Why not use the function "dot" ? 
-        dps[sum([10000000**i * dpsx[i] for i in range(1,3)])*10000000 + dpnn2] = [[x,y,z]]+nn2
+        dps[sum([1000000000**i * dpsx[i][0] for i in range(1,3)]) + dpnn2] = [[x,y,z]]+nn2
     return dps
 
-def gi(soo): # guess intersections.
-    M = [[0 for _ in range(len(soo))] for _ in range(len(soo))]
-    On =[[ [[],[]] for _ in range(len(soo))] for _ in range(len(soo))]
-    pairs = []
-    for i in range(len(soo)):
-        di = fi(soo[i])
-        for j in range(i+1,len(soo)):#  fi(soo[i]).keys() & fi(soo[j]).keys()
-            ct = 0
+def sams(d1, d2): # if dictionaries d1, d2 contain a common key, 
+    for k in d2:
+        if k in d1:
+            return k
+    return None
+
+def gi(i): #guess observation intersections. Use and write to global variables.
+    d1 = fi(soo[i])
+    for j in range(len(soo)):
+        if j not in reached: # fi(soo[i]).keys() & fi(soo[j]).keys()
             d2 = fi(soo[j])
-            for k in d2:
-                if k in di:
-                    ct += 1
-                    out = [di[k],d2[k]]
-            M[i][j] = ct
-            if ct >= 1:
-                On[i][j] = out
-                On[j][i] = [out[1],out[0]]
-                pairs.append([i,j])
-    return M, On, pairs
+            k = sams(d1,d2)
+            if k:
+                On[i][j] = [d1[k],d2[k]]
+                plan.append([i,j])
+                reached.append(j)
+                gi(j)
 
 def tr(ob0,ob1,sob):# translate into reference frame ob0 the observations sob made in frame ob1.
     E0 = [[1,0,0],[-1,1,0],[-1,0,1]]
@@ -89,70 +84,34 @@ def tr(ob0,ob1,sob):# translate into reference frame ob0 the observations sob ma
     for u in uni:
         if mm(E0,M0)[1:] == mm(mm(E0,M1),u)[1:]:
             ru = u # the right u
-    if ru is None: # if we didn't find the right unitary matrix, flip the rows.
-        E1 = mm([[1,0,0],[0,0,1],[0,1,0]], E0)
-        for u in uni:
-            if mm(E1,M0)[1:] == mm(mm(E1,M1),u)[1:]:
-                ru = u # the right u
+#    if ru is None: # if we didn't find the right unitary matrix, flip the rows.
+#        E1 = mm([[1,0,0],[0,0,1],[0,1,0]], E0)
+#        for u in uni:
+#            if mm(E1,M0)[1:] == mm(mm(E1,M1),u)[1:]:
+#                ru = u # the right u
     for [x,y,z] in sob:
         [[x0,y0,z0]] = mm([[x-ksx1, y-ksy1, z-ksz1]],ru)
         output.append([ksx0+x0,ksy0+y0,ksz0+z0])
     return output
 
-def rp(Pg): #from the graph Pg, make a reduction plan. 
-    planned = {}
-    plan = []
-    nn = {i:[] for i in range(len(soo)) if i not in planned}
-    for [x,y] in Pg:
-        if x not in planned and y not in planned:
-            nn[x].append(y)
-        if y not in planned and x not in planned:
-            nn[y].append(x)
-    len_queue = [(len(nn[k]),len(soo)-k) for k in nn]
-    len_queue.sort()
-    while len(len_queue) > 1:
-        plan_me = len(soo)-len_queue[0][1]
-        planned[plan_me] = 1
-        plan.append([plan_me,nn[plan_me][0]])
-        nn = {i:[] for i in range(len(soo)) if i not in planned}
-        for [x,y] in Pg:
-            if x not in planned and y not in planned:
-                nn[x].append(y)
-            if y not in planned and x not in planned:
-                nn[y].append(x)
-        len_queue = [(len(nn[k]),len(soo)-k) for k in nn]
-        len_queue.sort()
-    return plan
+def doit():
+    for ob0,ob1 in plan:
+        for y in tr(ob0,ob1,soo[ob1]):
+            if y not in soo[ob0]:
+                soo[ob0].append(y)
 
-with open('day19v0.txt', 'r') as file:
+with open('day19v1.txt', 'r') as file:
     te = [x.strip() for x in file.readlines()]
 
 soo = rev(te)
-M, On, Pg = gi(soo)
-[print(".".join([str(x) for x in row])) for row in M]
-
-so1 = [[y for y in x] for x in soo]
-plan = rp(Pg)
-
-for ob1,ob0 in plan:
-    for y in tr(ob0,ob1,so1[ob1]):
-        if y not in so1[ob0]:
-            so1[ob0].append(y)
-
-print("P1",len(so1[plan[-1][1]]))
-
-so1 = [[[0,0,0]] for x in soo]
-
-for ob1,ob0 in plan:
-    for y in tr(ob0,ob1,so1[ob1]):
-        if y not in so1[ob0]:
-            so1[ob0].append(y)
-
-scanners = so1[plan[-1][1]]
-
-dmax = 0
-for [x,y,z] in scanners:
-    for [a,b,c] in scanners:
-        dmax = max(abs(x-a)+abs(b-y)+abs(c-z), dmax)
-
-print("part2", dmax)
+On =[[ [[],[]] for _ in range(len(soo))] for _ in range(len(soo))]
+plan = []
+reached = [0]
+gi(0)
+plan.reverse()
+doit()
+print("P1",len(soo[plan[-1][0]]))
+soo = [[[0,0,0]] for x in soo]
+doit()
+scanners = soo[plan[-1][0]]
+print("part2", max([abs(x-a)+abs(b-y)+abs(c-z) for [a,b,c] in scanners for [x,y,z] in scanners]), time.time() - start)
